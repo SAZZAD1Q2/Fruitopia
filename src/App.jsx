23f53a3d58d0dec5871271a55d5b0dfdb06da494
SAZZAD1Q2 <kurulia115@gmail.com>
2024-09-07 13:44:04 +0600

    Drill props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Hello from "./components/Hello";
 function App() {
+  const person = {
+    name: 'Sazzad',
+    age: 23,
+    seatNumber: 35,
+    fruits: ["Mango","Apple","Jam"]
+  }
   return (
     <div className="App">
-     <Hello name = 'Sazzad' age = '23' />
+     <Hello person = {person} />
     </div>
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import PropTypes from "prop-types";
 
-const Hello = ({name, age}) => {
+const Hello = ({ person }) => {
   return (
-    <div>Hello {name}. I am {age} years old</div>
-  )
+    <div><h1>
+      Hello {person.name}. I am {person.age} and My seat number is {person.seatNumber}.I like to have {person.fruits.map((fruit) => fruit)}
+      
+    </h1></div>
+  );
+};
+Hello.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    seatNumber: PropTypes.number.isRequired,
+    fruits: PropTypes.string.isRequired,
+  })
 }
 
-export default Hello
+
+ 
+
+export default Hello;
